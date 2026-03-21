6f9b74cb1882a67013c6bd9ba942dd5a8a6b47b7
Swetha <dev02@acsass.com>
2026-03-21 12:24:30 +0530

    officer

--- routes/officers.js
+++ routes/officers.js
@@ -9,7 +9,9 @@ router.put('/:id', officerController.updateOfficer);
 router.delete('/:id', officerController.deleteOfficer); 
 
 // GET Routes for Department, Designation, Username
-router.get('/departments', officerController.getDepartments); 
+router.get('/departments', officerController.getDepartments);
+router.post("/departments", officerController.createDepartment);
+router.post("/designations", officerController.createDesignation);
 router.get('/designations', officerController.getDesignations); 
 router.get('/usernames', officerController.getUsernames); 
 
