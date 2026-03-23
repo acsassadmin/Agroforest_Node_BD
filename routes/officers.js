cc6ed5028a717161ced63ac6d62f937bfe579c84
Tamilvanan <tamil@acsass.com~>
2026-03-23 19:47:13 +0530

    Updated and created get call

--- routes/officers.js
+++ routes/officers.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const officerController = require('../controllers/Officer/officerController');  // Correct path
-// Officer Routes (Already Defined)
+const officerController = require('../controllers/Officer/officerController');
+
+
+
+
+
+router.post('/register', officerController.registerOfficer);
+
+
+router.get('/departments', officerController.getDepartments);
+router.post('/departments', officerController.createDepartment);
+
+router.get('/designations', officerController.getDesignations); 
+router.post('/designations', officerController.createDesignation);
+
+router.get('/usernames', officerController.getUsernames);
+
+
+
 router.get('/', officerController.getOfficers); 
 router.get('/:id', officerController.getOfficerById); 
-router.post('/', officerController.createOfficer); 
-router.put('/:id', officerController.updateOfficer); 
-router.delete('/:id', officerController.deleteOfficer); 
-
-// GET Routes for Department, Designation, Username
-router.get('/departments', officerController.getDepartments);
-router.post("/departments", officerController.createDepartment);
-router.post("/designations", officerController.createDesignation);
-router.get('/designations', officerController.getDesignations); 
-router.get('/usernames', officerController.getUsernames); 
+router.put('/:id', officerController.updateOfficer);
+router.delete('/:id', officerController.deleteOfficer);
 
 module.exports = router;
